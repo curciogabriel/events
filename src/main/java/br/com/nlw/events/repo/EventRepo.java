2441e649ccf75616a45fce70714e80c14b5ba612
curciogabriel <curcioog@gmail.com>
2025-02-18 17:50:18 -0300

    feat: add pattern layers, database SQL, REST API

---
+++ src/main/java/br/com/nlw/events/repo/EventRepo.java
@@ -0,0 +1,9 @@
+package br.com.nlw.events.repo;
+
+import br.com.nlw.events.model.Event;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventRepo extends CrudRepository<Event, Integer> {
+
+    public Event findByPrettyName(String prettyName);
+}
