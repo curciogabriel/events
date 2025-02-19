34b5e6163259473cdc945c71047ce4380d80154f
curciogabriel <curcioog@gmail.com>
2025-02-19 16:24:41 -0300

    feat: implemented registration manager

---
+++ src/main/java/br/com/nlw/events/model/Subscription.java
@@ -0,0 +1,61 @@
+package br.com.nlw.events.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_subscription")
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subscription_number")
+    private Integer subscriptonNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "subscribed_user_id")
+    private User subscriber;
+
+    @ManyToOne
+    @JoinColumn(name = "indication_user_id", nullable = true)
+    private User indication;
+
+    public Integer getSubscriptonNumber() {
+        return subscriptonNumber;
+    }
+
+    public void setSubscriptonNumber(Integer subscriptonNumber) {
+        this.subscriptonNumber = subscriptonNumber;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public User getSubscriber() {
+        return subscriber;
+    }
+
+    public void setSubscriber(User subscriber) {
+        this.subscriber = subscriber;
+    }
+
+    public User getIndication() {
+        return indication;
+    }
+
+    public void setIndication(User indication) {
+        this.indication = indication;
+    }
+
+    public Integer getSubscriptionNumber() {
+        return subscriptonNumber;
+    }
+}
